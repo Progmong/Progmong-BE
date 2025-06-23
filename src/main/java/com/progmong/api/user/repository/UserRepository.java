14f5a8e673fcedc3e40a235a967a2c71b319977e
P-WJ <pwj1103@gmail.com>
2025-06-23 17:57:49 +0900

    feat(Repository): User, PasswordReset

--- src/main/java/com/progmong/api/user/repository/UserRepository.java
+++ src/main/java/com/progmong/api/user/repository/UserRepository.java
@@ -3,5 +3,10 @@ package com.progmong.api.user.repository;
 import com.progmong.api.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByNickname(String nickname);
+
 }
