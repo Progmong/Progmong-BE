324c662189af60ee07c68f37b0d36128ebaa1a29
Heechan Hyun <hyeonhc328@gmail.com>
2025-06-23 16:31:31 +0900

    feat(explore): 사냥 기록 조회를 위한 레포지토리 및 기본 DTO 구현
    - ProblemRecordQueryDto 는 단순 조회에 해당해 레코드 클래스로 구현함

---
+++ src/main/java/com/progmong/api/explore/repository/ProblemRecordRepository.java
@@ -0,0 +1,14 @@
+package com.progmong.api.explore.repository;
+
+import com.progmong.api.explore.entity.ProblemRecord;
+import com.progmong.api.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProblemRecordRepository extends JpaRepository<ProblemRecord, Long> {
+
+    Page<ProblemRecord> findByUser(User user, Pageable pageable);
+
+    long countByUser(User user);
+}
