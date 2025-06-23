d680fd09d7371a824006e2a941ad322fe47cef25
Yoo-Hyuna <yha201341@gmail.com>
2025-06-23 10:59:31 +0900

    feat(pet): pet controller 추가 및 main에 @EnableJpaAuditing 추가
    - pet 등록을 위한 postApi 추가
    - BaseTimeEntity에 선언된 @CreatedDate, @LastModifiedDate가 정상 동작하려면 반드시 @EnableJpaAuditing이 필요
    - createdAt, updatedAt 자동 생성/업데이트를 위해 JPA Auditing 기능을 활성화.

--- src/main/java/com/progmong/ProgmongApplication.java
+++ src/main/java/com/progmong/ProgmongApplication.java
@@ -2,8 +2,10 @@ package com.progmong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ProgmongApplication {
 
 	public static void main(String[] args) {
