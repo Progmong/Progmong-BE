c8d05b8de63718be8b08462469bae36c9fbdf4e3
P-WJ <pwj1103@gmail.com>
2025-06-27 23:08:11 +0900

    feat(Project): 리프레시 토큰 추가

---
+++ src/main/java/com/progmong/api/user/dto/UserAccessTokenResponseDto.java
@@ -0,0 +1,12 @@
+package com.progmong.api.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserAccessTokenResponseDto {
+    private String accessToken;
+
+
+}
