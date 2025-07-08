f94469d672066c0d606781a03dab4eac63e86dfc
Choo ChangWoo <cnckddn0146@gmail.com>
2025-07-08 11:25:15 +0900

    fix(Community/Write): 게시글 JS 삽입 공격대비
    HTML을 그대로 불러오므로 예상외의 공격에 대비

--- src/main/java/com/progmong/api/community/util/HtmlSanitizer.java
+++ src/main/java/com/progmong/api/community/util/HtmlSanitizer.java
@@ -8,7 +8,12 @@ public class HtmlSanitizer {
         if (html == null) {
             return null;
         }
-        // 기본적인 텍스트 포맷용 태그들만 허용
-        return Jsoup.clean(html, Safelist.basic());
+//        // 기본적인 텍스트 포맷용 태그들만 허용
+//        return Jsoup.clean(html, Safelist.basic());
+
+        return Jsoup.clean(html, Safelist.relaxed()
+            .addTags("figure") // 추가로 figure 허용
+            .addAttributes("figure", "class", "style") // figure의 스타일과 클래스 허용
+            .addAttributes("img", "style")); // img에 style 허용
     }
 }
