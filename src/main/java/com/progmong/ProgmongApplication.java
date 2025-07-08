259e4d9da5c11d944b5130b3427c0c6113b7c773
taemin3 <xoals8534@gmail.com>
2025-07-08 20:26:48 +0900

    Update ProgmongApplication.java

--- src/main/java/com/progmong/ProgmongApplication.java
+++ src/main/java/com/progmong/ProgmongApplication.java
@@ -3,6 +3,8 @@ package com.progmong;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import java.util.TimeZone;
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableJpaAuditing
